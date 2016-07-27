fc568c3b05e012d006778927ae9e0b62a5ef928e
qnislavnachgev <qnislav.nachev@gmail.com>
2016-07-27 13:04:20 +0300

    This is the first task from testing: it test sumator!

---
+++ src/task1/Sumator.java
@@ -0,0 +1,10 @@
+package task1;
+
+public class Sumator {
+    public double sum(String a, String b) throws Exception {
+        if(a == null || b == null){
+            throw new IllegalArgumentException();
+        }
+        return Double.parseDouble(a) + Double.parseDouble(b);
+    }
+}
