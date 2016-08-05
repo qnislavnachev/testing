c503965d433923850fad5f3727d47bdb1283c577
qnislavnachgev <qnislav.nachev@gmail.com>
2016-08-05 09:28:22 +0300

    Fixed names of tests!

--- test/test/SumatorTest.java
+++ test/test/SumatorTest.java
@@ -10,22 +10,22 @@ public class SumatorTest {
     Sumator math = new Sumator();
 
     @Test
-    public void testSumatorWithNullNumbers() throws Exception {
+    public void SumWithZeroParameters() throws Exception {
         assertTrue(math.sum("0", "0") == 0);
     }
 
     @Test
-    public void testSumatorNotNullNumbers() throws Exception {
+    public void SumWithoutZeroParameters() throws Exception {
         assertTrue(math.sum("1", "3") == 4);
     }
 
     @Test(expected = NumberFormatException.class)
-    public void testSumatorExpectedNumberFormatException() throws Exception {
+    public void ExpectedNumberFormatException() throws Exception {
         assertTrue(math.sum("c", "5") == 5);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testSumatorExpectedIllegalArgumentException() throws Exception {
+    public void ExpectedIllegalArgumentException() throws Exception {
         double result = 5;
         assertEquals(math.sum(null, "5"), result, 0);
     }
